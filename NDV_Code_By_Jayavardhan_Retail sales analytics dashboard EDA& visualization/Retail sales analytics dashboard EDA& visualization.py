e7d1d8ef911812c46919aa727ddeea0646ace26f
Jayavardhan0 <jayavardhannaik6@gmail.com>
2025-06-14 16:11:32 +0530

    Create Retail sales analytics dashboard EDA& visualization.py
    No

---
+++ NDV_Code_By_Jayavardhan_Retail sales analytics dashboard EDA& visualization/Retail sales analytics dashboard EDA& visualization.py
@@ -0,0 +1,27 @@
+Retail sales analytics dashboard EDA& visualization:
+The goal of this project was to conduct a comprehensive Exploratory Data Analysis (EDA) on the 'SampleSuperstore' dataset, aiming to uncover insights that could assist a business manager in identifying weak areas and enhancing profitability. This analysis delved into order records to unveil trends, patterns, and factors that impact business operations and profits
+
+Project Summary:
+1. Total Orders: The dataset contained records of over 35,000 orders, providing a substantial volume of data for analysis.
+
+2. Total Sales: The cumulative sales from these orders amounted to over 2 million units in the local currency.
+
+3. Total Profit: The business yielded profits of approximately 286,000 units, reflecting its financial performance.
+
+4. Average Discount: Averaging a discount rate of 16%, the business implemented a pricing strategy to attract customers while maintaining profitability.
+
+5. Sales by Segment: The customer segment analysis revealed that consumers accounted for the majority of sales at 50.6%. Corporate customers contributed 30.7%, and Home Office customers represented 18.7% of sales.
+
+6. Sales by Ship Mode: The analysis of sales by shipping mode unveiled that "Standard Class" was the preferred choice for customers, comprising 59.1% of total sales. "Second Class" held a 20.0% share, "First Class" contributed 15.3%, and "Same Day" held 5.6% of sales.
+
+7. Sales by Region: A regional analysis indicated that the business thrived in specific regions. The "Central" region accounted for 21.8% of total sales, followed by "South" with 17.1%, "West" with 31.6%, and "East" with 29.5% of sales.
+
+8. Sales by Category: Among different product categories, "Furniture" emerged as the top-selling category, contributing sales of approximately 0.54 million units. "Technology" followed with sales of 0.33 million units, while "Office Supplies" accounted for sales of 0.22 million units.
+
+9. Sales by Sub-category: The analysis delved into sub-categories, revealing insights into the top-performing products. For the top 5 sub-categories, "Phones" led with sales of 0.33 million units, followed by "Chairs" with sales of 0.33 million units, "Storage" with sales of 0.22 million units, "Tables" with sales of 0.21 million units, and "Binders" with sales of 0.2 million units.
+
+10. Sales by Cities: The analysis identified the top-performing cities where the business achieved significant sales. The top 5 cities were New York City with sales of 256.4K units, Los Angeles with sales of 175.9K units, Seattle with sales of 119.5K units, San Francisco with sales of 112.7K units, and Philadelphia with sales of 109.1K units.
+
+Conclusion:
+This comprehensive Exploratory Data Analysis (EDA) of the 'SampleSuperstore' dataset provided valuable insights for the business manager. The findings include a detailed understanding of sales and profits, customer segments, preferred shipping modes, regional performance, top-selling product categories, sub-categories, and leading cities. These insights empower the business manager to formulate data-driven strategies to optimize weak areas, improve operational efficiency, and increase profitability.
+
